ac2f0698911f301ee0b61675e5cd206ad82aa2dd
MuzaffarJuraev <76548624+MuzaffarJuraev@users.noreply.github.com>
2021-05-10 11:20:38 +0500

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-// import TodoList from "./components/TodoList";
+//showing vs-code github setup
 export default class App extends Component {
   render() {
     return (
